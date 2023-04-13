85114d37586790ce1481811717423b8a9ed59bd7
msb332 <msb332@alumnos.unican.es>
2023-04-13 17:33:50 +0200

    ConexionServidor

--- main.c
+++ main.c
@@ -7,6 +7,6 @@ int main()
     	printf("Bienvenido a BeastVision");
 	Printf("El algoritmo ha sido desarrollado");
 	Printf("Llamadas de emergencia funcionando");
-    	
+    	Printf("La conexion con el servidor ha sido desarrollada");
 	return 0;
 }
