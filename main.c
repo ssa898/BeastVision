548a14764b2dcaef712f77f988b04295e50cb1ff
SERGIO <ssa898@alumnos.unican.es>
2023-04-13 17:21:41 +0200

    MainConSecciones

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
-//Codigo en blanco
+//Main con secciones
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
     	printf("Bienvenido a BeastVision");
+
+	//Rama de activacion de intrusos
+
+	//Rama de desactivacion de intrusos
+
+	//Rama de grabaciones
+
     	return 0;
 }
