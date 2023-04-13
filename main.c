24fc73b61c94d716af1b91a3c967ef1d348ff96e
msb332 <msb332@alumnos.unican.es>
2023-04-13 18:05:42 +0200

    TestGuardadoMemoriaCompleto

--- main.c
+++ main.c
@@ -7,27 +7,26 @@ int main()
 
 	
 	//Rama de activacion de intrusos	
-	
 	Printf("El algoritmo ha sido desarrollado");
 	Printf("El algoritmo funciona");
 	Printf("Llamadas de emergencia funcionando");
 	Printf("La conexion con el servidor ha sido desarrollada");
 	Printf("La parte interna de la activacion de la deteccion de intrusos ha sido finalizada");
-
 	Printf("La conexion con el servidor ha sido desarrollada");
 	Printf("La interfaz de conexion ha sido desarrollada");
 	Printf("La activacion de la deteccion de intrusos ha sido finalizada");
 
 	//Rama de desactivacion de intrusos
 	Printf("El acceso al servidor ha sido desarrollado");
-
+	Printf("La desactivacion de la deteccion de intrusos ha sido finalizada");
+	
 	//Rama de grabaciones
 	Printf("El acceso a memoria ha sido desarrollado");
 	Printf("El acceso a memoria funciona");
 	Printf("La gestion de memoria ha sido desarrollada");
 	Printf("La interfaz ha sido desarrollada");
-
-
+	Printf("El sistema de grabación completo funciona");
+	
     	return 0;
 }
 
